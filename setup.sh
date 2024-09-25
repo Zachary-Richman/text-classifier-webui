c307a648ced60e88933bd63217788d72b882a031
Zachary Richman <linuxuser@Bee1>
2024-09-25 17:23:10 -0400

    setup script and requirements

--- setup.sh
+++ setup.sh
@@ -1,4 +1,12 @@
 if [ ! python3 ]; then
-    echo "ERROR: Python3 is not installed"
-    exit 1
+	echo "ERROR: Python3 is not installed"
+   	exit 1
 else
+	echo "Python3 Installed."
+	git clone https://www.github.com/Zachary-Richman/text-classifier-webui
+	cd text-classifier-webui
+	python3 -m venv .venv
+	source ./.venv/bin/activate
+	pip install -r requirements.txt
+	pip list
+	echo "done."
